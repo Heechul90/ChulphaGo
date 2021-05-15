5a90404f651195bcc55ba7e3a35b9c6c22c6bfb5
heechullee <heechul4296@gmail.com>
2021-05-15 21:50:46 +0900

    superuser 생성

--- projects/chulsite/config/urls.py
+++ projects/chulsite/config/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('common.urls')),
-    path('board/', include('board.urls'))
+    path('board/', include('board.urls')),
 ]
